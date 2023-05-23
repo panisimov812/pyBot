256b6bd039c6f65176d65e1f92f6d5dffe32e71e
panisimov812 <petranisimov812@gmail.com>
2023-05-23 07:02:23 +0300

    добавил машиину состояний, для входа в админ панель

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,13 +1,12 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton#, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton  # , ReplyKeyboardRemove
 
 btnOpenHours = KeyboardButton('/Режим_работы')
 btnLocation = KeyboardButton('/Расположение')
 btnMenu = KeyboardButton('/Меню')
-#btnContact = KeyboardButton('/Контакты', request_contact=True)
-#btnMyLocation = KeyboardButton('/Поделиться где я',request_location=True)
+# btnContact = KeyboardButton('/Контакты', request_contact=True)
+# btnMyLocation = KeyboardButton('/Поделиться где я',request_location=True)
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
-kb_client.add(btnOpenHours).add(btnLocation).insert(btnMenu)#.row(btnContact,btnMyLocation)
-#kb_client.add(btnOpenHours).add(btnLocation).add(btnMenu)  #расположение друг под другом
-#kb_client.row(btnMenu,btnLocation,btnOpenHours)            #расположение в строку
-
+kb_client.add(btnOpenHours).add(btnLocation).insert(btnMenu)  # .row(btnContact,btnMyLocation)
+# kb_client.add(btnOpenHours).add(btnLocation).add(btnMenu)  #расположение друг под другом
+# kb_client.row(btnMenu,btnLocation,btnOpenHours)            #расположение в строку
